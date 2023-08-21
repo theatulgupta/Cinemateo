45594f4aa75e21de1874c6690b332e13adb4e7be
Atul <atulgupta3058@gmail.com>
2023-08-21 19:45:58 +0530

    Home Fixed

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -63,35 +63,35 @@ const MyCarousel = () => {
     return <Carousel
         autoPlay
         infiniteLoop
-        interval={ 1000 }
+        interval={ 2000 }
         showStatus={ false }
         showThumbs={ false }
         showArrows={ false }
     >
         <Box w={ 'full' }
             h={ '100vh' }>
-            <Image src={ img1 } />
+            <Image src={ img1 } h={ 'full' } w={ 'full' } objectFit={ 'cover' } />
             <Heading bgColor={ 'blackAlpha.600' } color={ 'white' } { ...headingOptions }>
                 Watch the future
             </Heading>
         </Box>
         <Box w={ 'full' }
             h={ '100vh' }>
-            <Image src={ img2 } />
+            <Image src={ img2 } h={ 'full' } w={ 'full' } objectFit={ 'cover' } />
             <Heading bgColor={ 'whiteAlpha.900' } color={ 'black' } { ...headingOptions }>
                 Future is Gaming
             </Heading>
         </Box>
         <Box w={ 'full' }
             h={ '100vh' }>
-            <Image src={ img3 } />
+            <Image src={ img3 } h={ 'full' } w={ 'full' } objectFit={ 'cover' } />
             <Heading bgColor={ 'whiteAlpha.600' } color={ 'black' } { ...headingOptions }>
                 Gaming on Console
             </Heading>
         </Box>
         <Box w={ 'full' }
             h={ '100vh' }>
-            <Image src={ img4 } />
+            <Image src={ img4 } h={ 'full' } w={ 'full' } objectFit={ 'cover' } />
             <Heading bgColor={ 'whiteAlpha.600' } color={ 'black' } { ...headingOptions }>
                 Thug Gaming
             </Heading>
